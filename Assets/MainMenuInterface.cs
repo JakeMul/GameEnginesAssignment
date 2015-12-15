4473dd81d6c45a85a04bb252015e054dd8588938
Jake Mulholland <eckojay2192@gmail.com>
2015-12-15 23:46:35 +0000

    Added quit option to visualizer

--- Assets/MainMenuInterface.cs
+++ Assets/MainMenuInterface.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class MenuScript : MonoBehaviour {
+public class MainMenuInterface : MonoBehaviour {
 
     public Canvas quitMenu;
     public Button startText;
---
+++ Assets/PlayerInterface.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerInterface : MonoBehaviour {
+
+    public Button exitText;
+
+    // Use this for initialization
+    void Start () {
+        exitText = exitText.GetComponent<Button>();
+        exitText.enabled = true;
+    }
+
+    public void exitLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
